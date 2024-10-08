fca291533a7b6e2bebc038cbb8f92a6688e14275
Yuri <coder73854@proton.me>
2024-10-07 23:46:08 -0300

    :book: studies: learn outputs

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World\n");
-    }
+  public static void main(String[] args) {
+    outputText();
+    outputNumber();
+  }
+
+  // Text Method
+  public static void outputText() {
+    // Print a character
+    System.out.print('A');   //
+    System.out.println('C'); //
+
+    // Print a string
+    System.out.print("Print without new line!");
+    System.out.print("\nPrint with new line\n");
+    System.out.println("Print with new line");
+  }
+
+  // Number Method
+  public static void outputNumber() {
+    System.out.print(10);   // print without a new line
+    System.out.println(10); // print with a new line
+
+    System.out.print(13 + 10); // we can make math operations and print the result
+  }
 }
---
+++ src/Output.java
@@ -0,0 +1,16 @@
+public class Output {
+  public static void outputText() {
+    // Print a character
+    System.out.print('A');   //
+    System.out.println('C'); //
+
+    // Print a string
+    System.out.print("Print without line!"); //
+    System.out.print("\nPrint with line\n");     // print with line
+    System.out.println("Print with line");     //
+  }
+
+  public static void outputNumber() {
+    
+  }
+}
